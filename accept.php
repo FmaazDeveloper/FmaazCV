58e0f0fdadd5319af88480cfb0442a9f5efaf42e
FmaazDeveloper <FmaazDeveloper@gmail.com>
2023-01-15 00:30:48 +0300

    add visitors counter

--- accept.php
+++ accept.php
@@ -2,6 +2,7 @@
 <html lang="en">
     <head>
         <?php require_once 'head.php' ;?>
+        <?php require_once 'connect_database.php' ;?>
     </head>
     <body>
 
@@ -12,7 +13,7 @@
                         '<center>
                             <form method="POST">
                                 <fieldset>
-                                    <legend> Enter the verification code sent to this email ( ' . $_SESSION['email'] . ' )</legend>
+                                    <legend> Enter the verification code sent to this email ( '.$_SESSION['email'].' )</legend>
                                     <div class="col-sm-3">
                                         <input type="number" name="code" class="form-control" id="code" placeholder="Enter verification code" required>
                                     </div>
@@ -35,10 +36,111 @@
 
             if(!empty($_POST['code']) && $_SESSION['code'] == $_POST['code'] && isset($_POST['check_code']))
                 {
-                    $_SESSION['check_code'] = $_POST['check_code'];
-                    $_SESSION['Ccode'] = $_POST['code'];
-                    echo '<center><br><div class="col-sm-3"><div class="alert alert-success" role="alert"> تم التحقق من الرمز بنجاح </div></div></center>';
-                    header("refresh:3; url= main.php");
+                    //Visitors count
+                    if($connect_database)
+                    {
+                        //Count date
+                        $select_count_date = $connect_database->prepare('SELECT MAX(count_date) count_date FROM visitors_count');
+                        $select_count_date->execute();
+
+                        foreach($select_count_date as $print)
+                            $_SESSION["count_date"] = $print["count_date"];
+
+                        if(empty($_SESSION["count_date"]))
+                            $_SESSION["count_date"] = "0000/00/00";
+
+                        //Visitors count
+                        $select_visitors_count = $connect_database->prepare('SELECT visitors_count FROM visitors_count WHERE count_date = "'.$_SESSION["count_date"].'"');
+                        $select_visitors_count->execute();
+    
+                        foreach($select_visitors_count as $print)
+                            $_SESSION["visitors_count"] = $print["visitors_count"];
+
+                            if(empty($_SESSION["visitors_count"]))
+                                $_SESSION["visitors_count"] = 1;
+                            elseif(!empty($_SESSION["visitors_count"]))
+                                $_SESSION["visitors_count"]++;
+
+                        if($_SESSION["count_date"] == $date)
+                        {
+                            if($_SESSION["visitors_count"] == 1)
+                            {
+                                $insert_visitors_count = $connect_database->prepare
+                                ('
+                                INSERT INTO visitors_count VALUES ( '.$_SESSION["visitors_count"].' , "'.$date.'" , "'.$time.'" )
+                                ');
+                                if($insert_visitors_count->execute())
+                                {
+                                    $_SESSION['check_code'] = $_POST['check_code'];
+                                    $_SESSION['Ccode'] = $_POST['code'];
+                                    echo '<center><br><div class="col-sm-3"><div class="alert alert-success" role="alert"> تم التحقق من الرمز بنجاح </div></div></center>';
+                                   header("refresh:3; url= main.php");
+                                }
+                                else
+                                {
+                                    echo '<center><br><div class="col-sm-3"><div class="alert alert-danger" role="alert">! حدث خطأ ما</div></div></center>';
+                                }
+                            }
+                            elseif($_SESSION["visitors_count"] > 1)
+                            {
+                                $update_visitors_count = $connect_database->prepare
+                                ('
+                                UPDATE visitors_count SET visitors_count = '.$_SESSION["visitors_count"].' WHERE count_date = "'.$date.'"
+                                ');
+                                if($update_visitors_count->execute())
+                                {
+                                    $_SESSION['check_code'] = $_POST['check_code'];
+                                    $_SESSION['Ccode'] = $_POST['code'];
+                                    echo '<center><br><div class="col-sm-3"><div class="alert alert-success" role="alert"> تم التحقق من الرمز بنجاح </div></div></center>';
+                                   header("refresh:3; url= main.php");
+                                }
+                                else
+                                {
+                                    echo '<center><br><div class="col-sm-3"><div class="alert alert-danger" role="alert">! حدث خطأ ما</div></div></center>';
+                                }
+                            }
+                        }
+                        elseif($_SESSION["count_date"] !== $date)
+                        {
+                            if($_SESSION["visitors_count"] == 1)
+                            {
+                                $insert_visitors_count = $connect_database->prepare
+                                ('
+                                INSERT INTO visitors_count VALUES ( '.$_SESSION["visitors_count"].' , "'.$date.'" , "'.$time.'" )
+                                ');
+                                if($insert_visitors_count->execute())
+                                {
+                                    $_SESSION['check_code'] = $_POST['check_code'];
+                                    $_SESSION['Ccode'] = $_POST['code'];
+                                    echo '<center><br><div class="col-sm-3"><div class="alert alert-success" role="alert"> تم التحقق من الرمز بنجاح </div></div></center>';
+                                   header("refresh:3; url= main.php");
+                                }
+                                else
+                                {
+                                    echo '<center><br><div class="col-sm-3"><div class="alert alert-danger" role="alert">! حدث خطأ ما</div></div></center>';
+                                }
+                            }
+                            elseif($_SESSION["visitors_count"] > 1)
+                            {
+                                $update_visitors_count = $connect_database->prepare
+                                ('
+                                UPDATE visitors_count SET visitors_count = '.$_SESSION["visitors_count"].' WHERE count_date = "'.$date.'"
+                                ');
+                                if($update_visitors_count->execute())
+                                {
+                                    $_SESSION['check_code'] = $_POST['check_code'];
+                                    $_SESSION['Ccode'] = $_POST['code'];
+                                    echo '<center><br><div class="col-sm-3"><div class="alert alert-success" role="alert">444 تم التحقق من الرمز بنجاح </div></div></center>';
+                                   header("refresh:3; url= main.php");
+                                }
+                                else
+                                {
+                                    echo '<center><br><div class="col-sm-3"><div class="alert alert-danger" role="alert">! حدث خطأ ما</div></div></center>';
+                                }
+                            }
+                        }
+                    }
+
                 }
             elseif (!empty($_POST['code']) && $_SESSION['code'] != $_POST['code'])
                     echo '<center><br><div class="col-sm-3"><div class="alert alert-danger" role="alert"> الرمز المدخل غير صحيح </div></div></center>';
--- nav.php
+++ nav.php
@@ -24,20 +24,35 @@ ob_start();
                 <li class="nav-item">
                     <a class="nav-link" style="color: black;" href="Projects.php">Projects | المشاريع</a>
                 </li>
-                &nbsp;&nbsp;&nbsp;&nbsp;_&nbsp;&nbsp;&nbsp;&nbsp;
                 <li class="nav-item">
                     <a class="nav-link" style="color: blue;" href="CV/FMAAZ CV.pdf" target="_blank">CV PDF</a>
                 </li>
-                <?php
+                
+                    <?php 
+                        require_once 'connect_database.php';
+                        if($connect_database)
+                        {
+                            $select_visitors_count = $connect_database->prepare('SELECT SUM(visitors_count) visitors_count FROM visitors_count');
+                            $select_visitors_count->execute();
+
+                            foreach($select_visitors_count as $print)
+                            $_SESSION["select_visitors_count"] = $print["visitors_count"];
+
+                            if(empty($_SESSION["select_visitors_count"]))
+                            $_SESSION["select_visitors_count"] = 0;
+
+                        }
+                        echo '
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                            <li class="nav-item"><a class="nav-link">Number of Visitors ('.$_SESSION["select_visitors_count"].') عدد الزوار</a></li>
+                        '; 
+
                     if($_SESSION['email'] == $_SESSION["admin_email"] && $_SESSION['name'] == $_SESSION["admin_password"])
                     {
                         echo 
                             '
-                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                                 <li class="nav-item" dir="rtl">
                                     <b><a class="nav-link" style="color: red;" href="admin.php">Admin page</a></b>
                                 </li>
